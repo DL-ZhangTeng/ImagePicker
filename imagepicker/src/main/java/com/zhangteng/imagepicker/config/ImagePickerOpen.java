6b08715d513817ea8007a481a84728114dfcd5f4
duoluo9 <763263368@qq.com>
2020-08-17 17:31:58 +0800

    修复请求权限失败时崩溃bug

--- imagepicker/src/main/java/com/zhangteng/imagepicker/config/ImagePickerOpen.java
+++ imagepicker/src/main/java/com/zhangteng/imagepicker/config/ImagePickerOpen.java
@@ -16,6 +16,7 @@ import com.zhangteng.imagepicker.activitys.CameraActivity;
 import com.zhangteng.imagepicker.activitys.CameraDialogFragment;
 import com.zhangteng.imagepicker.activitys.ImagePickerActivity;
 import com.zhangteng.imagepicker.utils.FileUtils;
+import com.zhangteng.imagepicker.utils.ToastUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,7 +56,7 @@ public class ImagePickerOpen {
 
                     @Override
                     public void failure() {
-                        openNoPermission(mActivity);
+                        ToastUtil.toastShort(mActivity, "请开启读写文件、相机和录制等相关权限！");
                     }
 
                     @Override
@@ -99,7 +100,7 @@ public class ImagePickerOpen {
 
                     @Override
                     public void failure() {
-                        openNoPermission(mActivity, requestCode);
+                        ToastUtil.toastShort(mActivity, "请开启读写文件、相机和录制等相关权限！");
                     }
 
                     @Override
